8a24d2b91eb0dedfad6a43fa810bbd95d5fe25e7
Rohit Bhoriya <rohit@kuants.in>
2024-03-13 16:19:47 +0530

    data

--- controllers/crudOps.controllers.js
+++ controllers/crudOps.controllers.js
@@ -12,7 +12,7 @@ const getdataController = (req, res) => {
   return getAllCategoryDataServiceegoryDataService(res);
 };
 const updatedataController = (req, res) => {
-  return UpdateCategoryDataService(req.parmas.id, res);
+  return UpdateCategoryDataService(req.parmas.id, req.body, res);
 };
 const deletedataController = (req, res) => {
   return DeleteCategoryDataService(req.params.id, res);
